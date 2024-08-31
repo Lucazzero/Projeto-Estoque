785908ace638b8c9df1a45fdf5080b853ad2ec79
Lucazzero <lucashenriquebrandino@gmail.com>
2024-08-30 21:58:40 -0400

    Encontro 4

--- ProjetoEstoque/src/visao/ClasseProdutoVisao.java
+++ ProjetoEstoque/src/visao/ClasseProdutoVisao.java
@@ -1,27 +1,24 @@
 package visao;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
 
 import dominio.ClasseProduto;
+import fakedb.ClasseProdutoFakeDB;
 
 public class ClasseProdutoVisao {
     public void Exibir(){
-        ClasseProduto cp1 = new ClasseProduto();
-        cp1.setCodigo(1);
-        cp1.setDescricao("Carnes");
-        cp1.setDataDeInclusao(LocalDate.now());
-
-        System.out.println("Classe de Produto:");
-        System.out.println("Codigo:" + cp1.getCodigo());
-        System.out.println("Descrição: "+ cp1.getDescricao());
-        System.out.println("Data de Inclusao: " + cp1.getDataDeInclusao());
-
-        ClasseProduto cp2 = new ClasseProduto(2, "Bebidas", LocalDate.now());
+        ClasseProdutoFakeDB fakeDB = new ClasseProdutoFakeDB();
+        ArrayList<ClasseProduto> tab = fakeDB.getTabela();
+        for (ClasseProduto cp : tab){
+            this.Imprimir(cp);
+        }
         
-        System.out.println("Classe de Produto:");
-        System.out.println("Codigo:" + cp2.getCodigo());
-        System.out.println("Descrição: "+ cp2.getDescricao());
-        System.out.println("Data de Inclusao: " + cp2.getDataDeInclusao());
     }
     
+    private void Imprimir(ClasseProduto alvo){
+        System.out.println("Classe de Produto:");
+        System.out.println("Codigo:" + alvo.getCodigo());
+        System.out.println("Descrição: "+ alvo.getDescricao());
+        System.out.println("Data de Inclusao: " + alvo.getDataDeInclusao());
+    } 
 }
